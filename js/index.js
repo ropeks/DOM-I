3b2ee894e89f9fe48b76738ef8114c0b9a1f53f5
ropeks <ropeksmilioner@gmail.com>
2019-05-13 15:50:49 +0200

    insert main, bottom and footer content

--- js/index.js
+++ js/index.js
@@ -64,4 +64,22 @@ h1.textContent = siteContent.cta["h1"]
 let button = document.querySelector("button");
 button.textContent = siteContent.cta["button"]
 
-// 
+// main, bottom and footer
+let h4 = document.querySelectorAll("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"]
+h4[1].textContent = siteContent["main-content"]["about-h4"]
+h4[2].textContent = siteContent["main-content"]["services-h4"]
+h4[3].textContent = siteContent["main-content"]["product-h4"]
+h4[4].textContent = siteContent["main-content"]["vision-h4"]
+h4[5].textContent = siteContent["contact"]["contact-h4"]
+
+let p = document.querySelectorAll("p");
+p[0].textContent = siteContent["main-content"]["features-content"]
+p[1].textContent = siteContent["main-content"]["about-content"]
+p[2].textContent = siteContent["main-content"]["services-content"]
+p[3].textContent = siteContent["main-content"]["product-content"]
+p[4].textContent = siteContent["main-content"]["vision-content"]
+p[5].textContent = siteContent["contact"].address
+p[6].textContent = siteContent["contact"].phone
+p[7].textContent = siteContent["contact"].email
+p[8].textContent = siteContent["footer"].copyright
